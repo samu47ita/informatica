7f0ec70ec6209343946f686a42e44eebee5bb475
samu47ita <xxsamu23@gmail.com>
2024-10-25 09:55:54 +0000

    update

---
+++ prove/prova_ciclo.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+    int s,c,num;
+    s=0
+    c=0
+    do
+    {
+        printf("inserisci nuneri interi")
+       scanf("%d"&num);
+       s=s+num;  
+       c=c++;
+    } while (c<10);
+    printf("la somma e uguale a: %d ",s);
+    printf("\n")
+ 
+    return 0
+    
+}
