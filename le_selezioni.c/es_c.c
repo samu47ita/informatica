4ce3f39bcc27ff9c23e44fe2ad1d15ef07cef45d
samu47ita <xxsamu23@gmail.com>
2024-10-30 10:48:35 +0000

    update

--- le_selezioni.c/es_c.c
+++ le_selezioni.c/es_c.c
@@ -1,26 +1,27 @@
 #include <stdio.h>
-int main
+int main ()
 {
-    int(spesa);
+     int spesa;
     printf("inserisci l'importo della spesa");
-    scanf("%d" &spesa);
+    scanf("%d", &spesa);
     if(spesa>50)
     {
         if(spesa>100)
         {
-            spesa=(spesa-(*10/100));
-            printf("sconto del 10% importo da pagare e: %d spesa");
+            spesa=(spesa-(spesa * 10/100));
+            printf("sconto del 10 percento importo da pagare e: %d",  spesa);
             printf("\n");
         }
         else
-        {
-            spesa=(spesa-(spesa*5/100));
-            printf("importo da pagare=%d"&spesa);
+        { 
+            spesa=(spesa-(spesa * 5/100));
+            printf("importo da pagare=%d",  spesa);
         }
+    }    
     else
-        {
-            printf("nesuno sconto aplicabile al importo da pagare e:%d"&spesa);
-        }       
-    }
-return 0;    
+    {
+      printf("nesuno sconto aplicabile al importo da pagare e: %d", spesa);
+    }       
+    
+    return 0;    
 }
