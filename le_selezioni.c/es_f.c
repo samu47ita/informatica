4a7d7ce27aa87b644e6534e56058eab773e0aa2d
samu47ita <xxsamu23@gmail.com>
2024-11-03 18:20:42 +0000

    ciao

---
+++ le_selezioni.c/es_f.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main()
+{
+    int media, crediti,voto_finale ;
+    printf("inserisci la media dello studente (valore intero): ");
+    scanf("%d", & media);
+
+    printf("iserisci i crediti dello studente: ");
+    scanf("%d", &crediti);
+
+    voto_finale = media;
+
+    if (media >= 28 && crediti >24)
+    {
+        voto_finale += 2 ;
+    }
+    else if (media >= 25 && media <= 27 && crediti >= 18 && crediti<= 24 );
+    {
+        voto_finale += 1;
+    }
+    printf("media iniziale: %d \n", media );
+    printf("crediti otenuti: %d \n", crediti);
+    printf("voto finale: %d \n", voto_finale);
+
+    return 0;
+}
