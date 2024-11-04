296acb813d91b2c95ad52a9b19bd7df2fc739441
samu47ita <xxsamu23@gmail.com>
2024-11-04 10:09:18 +0000

    update

---
+++ le_selezioni.c/es_g.c
@@ -0,0 +1,22 @@
+/*
+autore: Samuele poli
+Classe: 3^H
+data: 04/11/2024
+letto un carattere da tastiera visualizzare a schermo il carattere e i corrispondenti codici ascii in decimale e esadecimale
+
+input: caratere da tastiera
+elaborazione: codice ascii in decimale è esadecimale
+output: stampare schermo il carattere e i corrispondenti codici ascii in decimale e esadecimale
+*/
+
+# include <stdio.h>
+
+int main()
+{
+    int caratere;
+
+    printf("inserisci un caratere: ");
+    scanf("%d", &caratere );
+    printf("\n");
+    
+}
