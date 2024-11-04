399c025138ba62f16e4d88751e52e7f145136f42
samu47ita <xxsamu23@gmail.com>
2024-11-04 11:02:21 +0000

    esercizio h

---
+++ le_selezioni.c/es_h.c
@@ -0,0 +1,20 @@
+/*
+autore: Samuele poli
+Classe: 3^H
+data: 04/11/2024
+letto un carattere se corrisponde ad una lettera minuscola visualizzare la corrispondente maiuscola; 
+se invece è una lettera maiuscola visualizzare la corrispondente minuscola;
+ in tutti gli altri casi visualizzare “non è una lettera”;
+
+input: caratere da tastiera
+elaborazione: codice ascii in decimale è esadecimale
+output: stampare schermo il carattere e i corrispondenti codici ascii in decimale e esadecimale
+*/
+
+# include <stdio.h>
+
+int main()
+{
+ 
+    return 0;
+}
