7e117526dfce91d609afea56061791f645ef5f3b
samu47ita <xxsamu23@gmail.com>
2024-11-11 10:40:49 +0000

    update

---
+++ C-Dati,_Istruzioni_di_assegnazione_e_di_IO/ES02_polinomio.cpp
@@ -0,0 +1,68 @@
+/*
+	file: A_polinomio
+	autore: samuele poli
+	classe: 3H
+	data: 02/10/ 2024
+
+	risulozione di un polinomio
+
+	input: tre numeri interi
+	elaborazione: calcolo del' espresione
+	output. risultato del polinomio
+*/
+
+#include <stdio.h> //printf, scanf
+
+int main()
+{
+	int a = 7, b = 6, c = 5. x , y;
+	x = 2
+	int y = (a*x*x+b*x*x+c);
+	prinf ("x= %d allora y = %d \n",x,y);
+	
+	int a = 7, b = 6, c = 5. x , y;
+	x = 6
+	int y = (a*x*x+b*x*x+c);
+	prinf ("x= %d allora y = %d \n",x,y);
+	
+	int a = 7, b = 6, c = 5. x , y;
+	x = 8
+	int y = (a*x*x+b*x*x+c);
+	prinf ("x= %d allora y = %d \n",x,y);
+	
+	int a = 7, b = 6, c = 5. x , y;
+	x = 4
+	int y = (a*x*x+b*x*x+c);
+	prinf ("x= %d allora y = %d \n",x,y);
+
+	int a = 7, b = 6, c = 5. x , y;
+	x = 5
+	int y = (a*x*x+b*x*x+c);
+	prinf ("x= %d allora y = %d \n",x,y);
+	
+	int a = 7, b = 6, c = 5. x , y;
+	x = 9
+	int y = (a*x*x+b*x*x+c);
+	prinf ("x= %d allora y = %d \n",x,y);
+	
+	int a = 7, b = 6, c = 5. x , y;
+	x = 1
+	int y = (a*x*x+b*x*x+c);
+	prinf ("x= %d allora y = %d \n",x,y);
+	
+	int a = 7, b = 6, c = 5. x , y;
+	x = 3
+	int y = (a*x*x+b*x*x+c);
+	prinf ("x= %d allora y = %d \n",x,y);
+	
+	int a = 7, b = 6, c = 5. x , y;
+	x = 7
+	int y = (a*x*x+b*x*x+c);
+	prinf ("x= %d allora y = %d \n",x,y);
+	
+	int a = 7, b = 6, c = 5. x , y;
+	x = 0
+	int y = (a*x*x+b*x*x+c);
+	prinf ("x= %d allora y = %d \n",x,y);
+	return 0;
+}
---
+++ D-Istruzioni_decisionali_e_iterative/cicli_while_e_do_while/es_a_while.c
@@ -0,0 +1,15 @@
+/**
+ * scrivere un programma C che visualizzi sul monitor la media 
+ * di una sequenza di interi letti da tastiera. La sequenza 
+ * termina appena si digita zero 
+ * 
+ * @author Samuele Poli, 3H
+ * @date 11/11/24
+ */
+
+#include <stdio.h>
+
+int main ()
+{
+    printf("")
+}
