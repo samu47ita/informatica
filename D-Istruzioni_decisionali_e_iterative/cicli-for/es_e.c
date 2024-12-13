46cbd04451da1604c35b6255482e23a071231f26
samu47ita <xxsamu23@gmail.com>
2024-12-13 10:10:07 +0000

    update

---
+++ D-Istruzioni_decisionali_e_iterative/cicli-for/es_e.c
@@ -0,0 +1,31 @@
+/*  
+ * @author Samuele Poli, 3H
+ * @date 13/12/24
+ * 
+ * esercizio:scrivere un programma che calcoli il fattoriale di un numero intero N positivo dato dalla tastiera
+ * 
+ * input:letto un numero intero da tastiera 
+ *elaborazione:calcolo del fatoriale
+ *output: risultato del fatoriale
+ */
+
+#include <stdio.h>
+
+int main()
+{
+
+    int n,fattoriale,cont;
+    fattoriale=1;
+
+    printf("inserisci un numero intero positivo\n");
+    scanf("%d",&n);
+
+    for (cont = 1;cont <= n ;cont++)
+    {
+        fattoriale *= cont;
+    }
+    printf("ilfattorile di %d è:%d \n",n,fattoriale);
+
+return 0;
+
+}
---
+++ D-Istruzioni_decisionali_e_iterative/cicli-for/es_f.c
@@ -0,0 +1,12 @@
+/*  
+ * @author Samuele Poli, 3H
+ * @date 13/12/24
+ * 
+ * esercizio:Scrivere un programma che stampi tutti i numeri primi compresi tra 1 e 100. Un numero è primo quando ha come divisore uno e sé stesso.
+ *  Quindi è primo ciascun numero naturale maggiore di 1 che sia divisibile solamente per 1 e per sé stesso. 
+ * 
+ * input:
+ *elaborazione:
+ *output:
+ */
+#include <stdio.h>
