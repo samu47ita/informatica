15cedc9fa2b6fb6df84a1fe82d6051605432efad
samu47ita <xxsamu23@gmail.com>
2025-03-10 10:46:33 +0000

    update

--- Informatica/arrary_vettori/es_a2.c
+++ Informatica/arrary_vettori/es_a2.c
@@ -1,8 +1,33 @@
-/* @brief dichiari un array di 10 interi 
- * @param vetore
- * @param costante N
+/* @brief 
+ * @param 
+ * @param 
  *  
  * @author Samuele Poli, 3H
  * @date 10/03/25
 */
 #include<stdio.h>
+#define N 10
+
+int main()
+{
+    float vet[N];
+    float somma , media;
+    for(int cont =0; cont < N; cont ++ )
+    {
+        printf("inserisci un numero:");
+        scanf("%f",&vet[cont]);
+
+    }
+
+
+    for(int cont =0; cont < N; cont ++ )
+    {
+        somma += vet[cont];
+    }
+    printf("la somma dei numeri é: %.1f \n", somma);
+    
+    media = somma / N;
+    printf("la media dei numeri é: %.1f \n", media);
+
+    return 0;
+}
