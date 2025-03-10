96560d9fe949a0fd6636f5a0bd4a096b9bac1a61
samu47ita <xxsamu23@gmail.com>
2025-03-10 10:22:01 +0000

    update

--- Informatica/arrary_vettori/es_1.c
+++ Informatica/arrary_vettori/es_1.c
@@ -1,9 +1,10 @@
-/*
+/* @brief dichiari un array di 10 interi 
+ * @param vetore
+ * @param costante N
+ *  
+ * @author Samuele Poli, 3H
+ * @date 11/11/24
 */
-
-
-
-
 #include<stdio.h>
 
 #define N 10
@@ -11,6 +12,7 @@
 
 int main()
 {
+    
     int vet[N] = {1,2,3,4,5,6,7,8,9,10};
 
     for(int cont =0; cont < N; cont ++ )
