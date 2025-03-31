485df668b190b1f1594728c303e851c71775e680
samu47ita <xxsamu23@gmail.com>
2025-03-31 09:07:30 +0000

    update

--- Informatica/arrary_vettori/es_b1.c
+++ Informatica/arrary_vettori/es_b1.c
@@ -2,6 +2,43 @@
 #include<stdlib.h>
 #include<time.h>
 
+void stampa_array(int V[]);
+void istogramma(int V[]);
 
 
-#
+#define N 10
+
+int main()
+{
+    int vet[N];
+
+
+    srand(time(NULL));
+    
+    for (int i = 0; i < N; i++)
+    {
+        vet[i] = rand() % 20;
+    }
+
+}
+void istogramma(int V[])
+{
+    for(int i = 0; i < N; i++)
+    {
+        printf("%d:",V[i]);
+         
+    }
+}
+
+
+
+
+void stampa_array(int V[])
+{ 
+    for (int i = 0; i < N; i++)
+    {
+     printf("%d ",V[i]);
+     printf("\n");
+    }
+
+}
