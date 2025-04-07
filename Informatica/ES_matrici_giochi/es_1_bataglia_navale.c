b5a75bff0a8b6072eb8e83a4ce413bfd4d9af4bf
samu47ita <xxsamu23@gmail.com>
2025-04-07 09:13:36 +0000

    update

--- Informatica/ES_matrici_giochi/es_1_bataglia_navale.c
+++ Informatica/ES_matrici_giochi/es_1_bataglia_navale.c
@@ -23,10 +23,7 @@ void visualizzaCampo(char campo[][DIMENSIONE]);
 
 int main()
 {
-    char campo[DIMENSIONE][DIMENSIONE];
-
-
-     
+    char campo[DIMENSIONE][DIMENSIONE];  
     inizializzaCampo(campo);
     visualizzaCampo(campo);
 
