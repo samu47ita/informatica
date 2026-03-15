ccdbe5d3930fe9a7cf2c1131aeb9ca81e2b22b82
samu47ita <xxsamu23@gmail.com>
2026-03-15 16:53:37 +0000

    update

--- anno_4H/cpp/coda.cpp
+++ anno_4H/cpp/coda.cpp
@@ -11,11 +11,11 @@ struct coda
 void enqueue(coda &c, int elemento)
 {
     c.A[c.j] = elemento;
-    c.j = (c.j + 1) % 10; 
+    c.j = (c.j + 1);
 }
 int dequeue(coda &c){
     int elemento = c.A[c.i];
-    c.i = (c.i + 1) % 10; 
+    c.i = (c.i + 1);  
     return elemento;
 }
 bool isEmpty(coda c){
@@ -26,12 +26,12 @@ int main()
     coda c;
     c.i = 0; // Inizializza la coda come vuota
     c.j = 0; // Inizializza j a 0
-    enqueue(c, 5);
-    enqueue(c, 10);
-    enqueue(c, 15);
-    cout << dequeue(c) << endl; // Output: 5
-    cout << dequeue(c) << endl; // Output: 10
-    cout << dequeue(c) << endl; // Output: 15
+    enqueue(c, 1);
+    enqueue(c, 2);
+    enqueue(c, 3);
+    cout << dequeue(c) << endl; // Output: 1
+    cout << dequeue(c) << endl; // Output: 2
+    cout << dequeue(c) << endl; // Output: 3
     cout << isEmpty(c) << endl; // Output: 1 (true)
     return 0;
 }
